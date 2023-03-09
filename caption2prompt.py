ecdc3b51bf448fc7b831eafe0437d368ddae2c41
yomero <yo@mimismo.com>
2023-03-09 18:44:43 +0000

    First commit

---
+++ caption2prompt.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import os
+import random
+import argparse
+
+def clean_filename(filename):
+    return filename.split('_')[0]
+
+def get_filenames_from_target_folders(input_folder, target_folder):
+    target_folders = []
+    for root, dirs, files in os.walk(input_folder):
+        if target_folder in dirs:
+            target_folders.append(os.path.join(root, target_folder))
+    filename_list = []
+    for path in target_folders:
+        for root, dirs, files in os.walk(path):
+            for filename in files:
+                filename_list.append(clean_filename(filename))
+    return filename_list
+
+
+def main(filename, input_folder, target_folder, seed, samples, duplicate):
+    """
+    Create a XYZ prompt tests from caption filenames.
+
+    Examples:
+    $ python3 caption2prompt.py -d
+    $ python3 caption2prompt.py -i input -t blip -S 15 --seed -1 -d
+    """
+    caption_list = random.sample(get_filenames_from_target_folders(input_folder, target_folder), samples)
+
+    # Extend caption list including captions without token
+    caption_without_token = []
+    if duplicate:
+        for caption in caption_list:
+            caption_without_token.append(caption.split(', ')[-1])
+
+    prompt_list = caption_list + caption_without_token
+
+    # Prepare data dict with captions as prompts
+    data = []
+    for prompt in prompt_list:
+        d = {}
+        d['prompt'] = prompt
+        d['prompt_sr'] = '' 
+        d['z_axis_type'] = 'Nothing'
+        d['seed'] = seed
+        data.append(d)
+
+    # Save prompts to json file
+    with open(filename, 'w') as fp:
+        json.dump(data, fp)
+
+
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', type=str, default='xyz_prompt_tests.json', help='The name of the tests JSON file')
+    parser.add_argument('-i', '--input_folder', type=str, default='input', help='The root folder where subfolders with caption filenames are located')
+    parser.add_argument('-t', '--target_folder', type=str, default='blip', help='The name of the folder that contains caption filenames')
+    parser.add_argument('--seed', type=int, default=555, help='Seed value')
+    parser.add_argument('-S', '--samples', type=int, default=15, help='Number of filenames to generate')
+    parser.add_argument('-d', '--duplicate', action="store_true", default=False, help='Duplicate every caption without its token (Def: True)')
+    args = parser.parse_args()
+
+    filename = args.filename
+    input_folder = args.input_folder
+    target_folder = args.target_folder 
+    seed = args.seed 
+    samples = args.samples 
+    duplicate = args.duplicate
+
+	# Run main
+    main(filename, input_folder, target_folder, seed, samples, duplicate)
+
---
+++ prompt2test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import argparse
+
+
+def main(filename, prompt, prompt_sr, seed):
+    """
+    Create or add Prompt S/R tests to a JSON file.
+
+    Examples:
+    $ prompt2test.py -p 'A scene from 10 Cloverfield Lane' -r '10 Cloverfield Lane, nightmare on elm street, matrix, star wars, 20th century women, toy story'
+    $ prompt2test.py -p 'A photo of a cartoon character' -r 'cartoon character, monkey, dog, cat, statue, painting, pottery, car, house, city'
+    $ prompt2test.py -p 'A photo of Charlize Theron' -r 'Charlize Theron, Morgan Freeman, Michelle Obama, Mark Zuckerberg, Rihanna, emma watson, person, man, woman, boy, girl'
+    """
+    # Open tests file
+    try:
+        with open (filename, 'r') as f:
+            data = json.loads(f.read())
+    except Exception:
+        data = []
+    # Add test to file
+    data.extend([{
+         'prompt': prompt, 
+         'prompt_sr': prompt_sr,
+         'seed': seed,
+         'z_axis_type': "Prompt S/R"
+    }])
+    # Save prompts to JSON file
+    with open(filename, 'w') as fp:
+        json.dump(data, fp)
+
+
+if __name__ == "__main__":
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', type=str, default='prompt_sr_tests.json', help='The name of the tests JSON file')
+    parser.add_argument('-p', '--prompt', type=str, help='Prompt')
+    parser.add_argument('-r', '--prompt_sr', type=str, help='Prompt S/R')
+    parser.add_argument('--seed', type=int, default=555, help='Seed')
+    args = parser.parse_args()
+
+    filename = args.filename
+    prompt = args.prompt
+    prompt_sr = args.prompt_sr
+    seed = args.seed
+
+    main(filename, prompt, prompt_sr, seed)
+
